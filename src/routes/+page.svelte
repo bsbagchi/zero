69c4ef1933b0d8a1e5266bf3dddb6cfbf6d3f94b
bsbagchi <brotisunder9@gmail.com>
2024-12-28 22:27:49 +0530

    hero my zero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
+    const zero =[{"img":'../multimarket.svg'},{"img":'../multimarket.svg'},{"img":'../multimarket.svg'}]
 
-    const hero='../multimarket.svg';
 </script>
-
+{#each zero as item }
 <div class="w-40 h-40 bg-black flex items-center justify-center">
     
-    <img src={new URL(hero,import.meta.url).href} class="w-20 h-20" alt="An alt text" />
+    <img src={new URL(item.img,import.meta.url).href} class="w-20 h-20" alt="An alt text" />
 
 </div>
+{/each}
+
   
