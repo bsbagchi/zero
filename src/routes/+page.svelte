d38d34a8de80da6f208716253288bd3f7c1f90f3
bsbagchi <brotisunder9@gmail.com>
2024-12-28 21:55:32 +0530

    nothing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+
+    const hero='../multimarket.svg';
+</script>
+
 <div class="w-40 h-40 bg-black flex items-center justify-center">
-    <enhanced:img src="/src/multimarket.svg" class="w-20 h-20" alt="An alt text" />
-  </div>
+    
+    <img src={new URL(hero,import.meta.url).href} class="w-20 h-20" alt="An alt text" />
+
+</div>
   
