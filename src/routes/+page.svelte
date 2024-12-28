2228d9415e0a0408659b3371f3ec323db9f76bd3
mranv <iamanubhavgain@gmail.com>
2024-12-28 23:23:13 +0530

    remove unused multimarket.svg and update image source in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 {#each zero as item }
 <div class="w-40 h-40 bg-black flex items-center justify-center">
     
-    <img src={new URL(item.img,import.meta.url).href} class="w-20 h-20" alt="An alt text" />
+    <img src={item.img} class="w-20 h-20" alt="An alt text" />
 
 </div>
 {/each}
